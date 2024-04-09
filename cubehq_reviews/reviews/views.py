9bb48d65ad24b4396b7e42ae51a4c298858f5b57
jarvis <dhruv1778@gmail.com>
2024-04-09 13:57:14 +0530

    Use ListAPIView instead of APIView

--- cubehq_reviews/reviews/views.py
+++ cubehq_reviews/reviews/views.py
@@ -1,40 +1,53 @@
 from django.db.models import Avg, Count, F, OuterRef, Subquery
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import CursorPagination
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from .models import Metadata, ReviewHistory
-from .serializers import TopCategorySerializer, LatestReviewSerializer
+from reviews.models import Metadata, ReviewHistory
+from reviews.serializers import TopCategorySerializer, LatestReviewSerializer
 
 
-class ReviewTrendView(APIView):
-    def get(self, request):
-        top_categories = (
-            Metadata.objects.filter(
+class ReviewTrendView(ListAPIView):
+    """Lists Top Review Categories."""
+    
+    serializer_class = TopCategorySerializer
+    queryset = Metadata.objects
+
+    def filter_queryset(self, queryset):
+        """Filters the top 5 categories along with total review count and average stars."""
+        TOP_CATEGORY_COUNT = 5  # No. of top categories to be filtered
+
+        # Logic to get top categories based on average star rating
+        top_categories_queryset = (
+            queryset.filter(
                 review_id=Subquery(
                     ReviewHistory.objects.filter(id=OuterRef("review_id"))
                     .order_by("-created_at")
-                    .values("id")[:1]
+                    .values("id")[
+                        :1
+                    ]  # Gets the latest review for a particular review_id
                 )
             )
-            .values('category_id', name=F('category__name'), description=F('category__description'))
+            .values(
+                "category_id",
+                name=F("category__name"),
+                description=F("category__description"),
+            )
             .annotate(average_stars=Avg("review__stars"), total_reviews=Count("review"))
-            .order_by("-average_stars")
+            .order_by("-average_stars")[:TOP_CATEGORY_COUNT]
         )
-        print(top_categories)
-        serializer = TopCategorySerializer(top_categories, many=True)
-        return Response(serializer.data)
+        # This took a total of 2 django ORM queries(Including 1 Subquery)
+        return top_categories_queryset
+
+
+class ReviewSearchPagination(CursorPagination):
+        """Sets pagination parameters for filtering reviews."""
+
+        page_size = 15
+        ordering = "-created_at"
 
 
 class CategoryReviewView(ListAPIView):
-    """
-    Fetches reviews filtered by a provided category ID.
-    """
-
-    class ReviewSearchPagination(CursorPagination):
-        page_size = 5
-        ordering = "-id"
+    """Lists reviews filtered by a provided category ID."""
 
     pagination_class = ReviewSearchPagination
     queryset = Metadata.objects
@@ -42,12 +55,11 @@ class CategoryReviewView(ListAPIView):
 
     def filter_queryset(self, queryset):
         """
-        Filters the queryset based on a 'category_id' query parameter.
+        Filter the queryset based on a 'category_id' query parameter.
 
-        If category_id is not present, filters all reviews
+        If category_id is not present, returns all reviews
         """
-
         category_id = self.request.GET.get("category_id")
         if category_id is not None:
             queryset = queryset.filter(category_id=category_id)
-        return queryset
+        return queryset.annotate(created_at=F("review__created_at"))
